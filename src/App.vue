fb42e5b58863f67698da23f8e74cbc04bacab696
Muhammadamin <webhacker0100@gmail.com>
2022-02-24 23:24:02 +0500

    Created content.

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,39 @@ export default {
 </script>
 
 <template>
+<br>
+ <div class="container">
+   <div class="card center">
+     <h2>Your IP address info</h2>
+     <div class="flex card">
+       <div class="box">
+         <span>Hello</span>
+         <br>
+         <span>1111</span>
+       </div>
+       <div class="box">
+         <span>Hello</span>
+         <br>
+         <span>1111</span>
+       </div>
+       <div class="box">
+         <span>Hello</span>
+         <br>
+         <span>1111</span>
+       </div>
+       <div class="box">
+         <span>Hello</span>
+         <br>
+         <span>1111</span>
+       </div>
+       <div class="box">
+         <span>Hello</span>
+         <br>
+         <span>1111</span>
+       </div>
+     </div>
+   </div>
+ </div>
 </template>
 
 <style>
